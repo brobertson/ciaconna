1631583a017b756ff7c29224ffc57ab61e7c2008
Bruce Robertson <bruce.g.robertson@gmail.com>
2014-05-23 10:59:40 -0400

    beautified and parameterized bash scripts

--- bin/ocropus_batch.sh
+++ bin/ocropus_batch.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 export PYTHONIOENCODING=UTF-8
-#set the directory of the collection of books
-#COLLECTION_DIR=/usr/local/OCR_Processing/Texts/Athenaeus/Pngs
 # We expect two args: the collection_dir and the classifier_dir
-USAGE_MESSAGE="Usage: ocropus_batch pdf_file classifier_file"
+USAGE_MESSAGE="Usage: ocropus_batch [pdf_file,zip file]  classifier_file"
 EXPECTED_ARGS_MIN=2
 EXPECTED_ARGS_MAX=2
 E_BADARGS=65
@@ -15,6 +13,11 @@ PPI=400
 #check that env. variables are set
 [ -z "$CIACONNA_HOME" ] && { echo "Need to set CIACONNA_HOME env. variable"; exit 1; }
 
+if [ ! -d "$OCR_OUTPUT_DIR" ]; then
+  echo "Need to set OCR_OUTPUT_DIR env. variable" 
+  exit 1
+fi
+
 #set date
 FOO=${DATE:=`date +%F-%H-%M`}
 echo "Using Date $DATE"
@@ -25,14 +28,14 @@ then
   exit $E_BADARGS
 fi
 if [ ! -f $CLASSIFIER_FILE ]; then
-	echo "classifier file $CLASSIFIER_FILE does not exist"
-        echo $USAGE_MESSAGE
-        exit $E_BADARGS
+  echo "classifier file $CLASSIFIER_FILE does not exist"
+  echo $USAGE_MESSAGE
+  exit $E_BADARGS
 fi
 if [ ! -f "$INPUT_FILE" ]; then
-        echo "pdf file $INPUT_FILE does not exist"
-        echo $USAGE_MESSAGE
-        exit $E_BADARGS
+  echo "pdf file $INPUT_FILE does not exist"
+  echo $USAGE_MESSAGE
+  exit $E_BADARGS
 fi
 
 file=${INPUT_FILE##*/}
@@ -43,45 +46,45 @@ ext=${file##*.}
 
 
 if [ ! "$ext" == "pdf" ] && [ ! "$ext" == "zip"]; then
-	echo "pdf file $INPUT_FILE does not have a recognized extension: $ext"
-	echo $USAGE_MESSAGE
-        exit $E_BADARGS
+  echo "pdf file $INPUT_FILE does not have a recognized extension: $ext"
+  echo $USAGE_MESSAGE
+  exit $E_BADARGS
 fi
 
 if [ ! -d $OCR_OUTPUT_DIR/$base ]; then
-	echo "working directory $OCR_OUTPUT_DIR/$base does not exist. Making it."
-	mkdir "$OCR_OUTPUT_DIR/$base"
+  echo "working directory $OCR_OUTPUT_DIR/$base does not exist. Making it."
+  mkdir "$OCR_OUTPUT_DIR/$base"
 fi
 IMAGE_DIR=$OCR_OUTPUT_DIR/$base/$base-PNG-$PPI
 
 if [  "$ext" == "pdf" ]; then
-if [ ! -d $IMAGE_DIR ]; then
-	echo "converting pdf to pngs at resolution $PPI..."
-	mkdir $IMAGE_DIR
-	cd $IMAGE_DIR
-        pdftoppm -r $PPI  -png $INPUT_FILE $base
-	echo "done converting" 
-fi
+  if [ ! -d $IMAGE_DIR ]; then
+    echo "converting pdf to pngs at resolution $PPI..."
+    mkdir $IMAGE_DIR
+    cd $IMAGE_DIR
+    pdftoppm -r $PPI  -png $INPUT_FILE $base
+    echo "done converting"
+  fi
 fi
 
 if [ "$ext" == "zip" ]; then
-if [ ! -d $IMAGE_DIR ]; then
-	echo "uncompressing zip file"
-	mkdir $IMAGE_DIR
-        cd $IMAGE_DIR
-	unzip "$INPUT_FILE"
-	echo "done uncompressing"
-	for image_file in $(ls *_jp2/*.jp2 *_tif/*.tif)
-	do
-		b=$(basename $image_file)
-		filebase=${b%.*}
-       	 	fileext=${b##*.}
-                if [ ! -f $filebase.png ]; then 
-			echo "converting $image_file to $filebase.png"
-			convert $image_file $filebase.png
-		fi
-	done
-fi
+  if [ ! -d $IMAGE_DIR ]; then
+    echo "uncompressing zip file"
+    mkdir $IMAGE_DIR
+    cd $IMAGE_DIR
+    unzip "$INPUT_FILE"
+    echo "done uncompressing"
+    for image_file in $(ls *_jp2/*.jp2 *_tif/*.tif)
+    do
+      b=$(basename $image_file)
+      filebase=${b%.*}
+      fileext=${b##*.}
+      if [ ! -f $filebase.png ]; then
+        echo "converting $image_file to $filebase.png"
+        convert $image_file $filebase.png
+      fi
+    done
+  fi
 fi
 classifier_file_base=$(basename $CLASSIFIER_FILE)
 INNER_HOCR_OUTPUT_DIR=${DATE}_${classifier_file_base}_raw_hocr_output
@@ -89,31 +92,31 @@ INNER_SELECTED_DIR=${DATE}_${classifier_file_base}_selected_hocr_output
 HOCR_OUTPUT_DIR=$IMAGE_DIR/$INNER_HOCR_OUTPUT_DIR
 SELECTED_DIR=$IMAGE_DIR/$INNER_SELECTED_DIR
 if [ ! -d $HOCR_OUTPUT_DIR ]; then
-        echo "making hocr output dir"
-        mkdir $HOCR_OUTPUT_DIR
-	mkdir $SELECTED_DIR
+  echo "making hocr output dir"
+  mkdir $HOCR_OUTPUT_DIR
+  mkdir $SELECTED_DIR
 fi
 
 SMALL_IMAGE_DIR=$IMAGE_DIR/${base}_color
 if [ ! -d $SMALL_IMAGE_DIR ]; then
-        echo "making jpg output dir"
-        mkdir $SMALL_IMAGE_DIR
+  echo "making jpg output dir"
+  mkdir $SMALL_IMAGE_DIR
 fi
 
 cd "$IMAGE_DIR"
 echo "processing image files"
 for image_file in $(ls *.png)
 do
-	filebase=${image_file%.*}
-	fileext=${image_file##*.}
-	if [ ! -f $HOCR_OUTPUT_DIR/$filebase.html ]; then
-		echo "processing $image_file into $filebase.html"
-		$CIACONNA_HOME/bin/ocropus_page.sh -v -l $CLASSIFIER_FILE -o $HOCR_OUTPUT_DIR/$filebase.html $image_file 2>&1
-	fi
-	if [ ! -f $SMALL_IMAGE_DIR/$file_base.jpg ]; then
-		echo "creating $file_base.jpg"
-		convert $image_file -scale 30% $SMALL_IMAGE_DIR/$filebase.jpg
-	fi
+  filebase=${image_file%.*}
+  fileext=${image_file##*.}
+  if [ ! -f $HOCR_OUTPUT_DIR/$filebase.html ]; then
+    echo "processing $image_file into $filebase.html"
+    $CIACONNA_HOME/bin/ocropus_page.sh -v -l $CLASSIFIER_FILE -o $HOCR_OUTPUT_DIR/$filebase.html $image_file 2>&1
+  fi
+  if [ ! -f $SMALL_IMAGE_DIR/$file_base.jpg ]; then
+    echo "creating $file_base.jpg"
+    convert $image_file -scale 30% $SMALL_IMAGE_DIR/$filebase.jpg
+  fi
 done
 echo "Classifier file base: $classifier_file_base"
 archive_name_base="robertson_${DATE}_${base}_${classifier_file_base}_full"
--- bin/ocropus_page.sh
+++ bin/ocropus_page.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 usage(){
-	echo "Usage: $0 -l classifier_model -o output_filename [-v -p] input_filename"
-	echo "-v verbose output"
-	echo "-p Image has already been processed, useful for paralleling output"
-	exit 1
+  echo "Usage: $0 -l classifier_model -o output_filename [-v -p] input_filename"
+  echo "-v verbose output"
+  echo "-p Image has already been processed, useful for paralleling output"
+  exit 1
 }
 
 delete_string=" &> /dev/null "
@@ -15,101 +15,103 @@ columns_command=""
 while getopts ":l:o:C:c:vp" opt; do
   case $opt in
     v)
-	    delete_string=""
-	    verbose=true
-	 ;;
+      delete_string=""
+      verbose=true
+    ;;
     p)
-	    image_is_preprocessed=true
-	    ;;
+      image_is_preprocessed=true
+    ;;
     o)
-     output_filename=$OPTARG
-     ;;
+      output_filename=$OPTARG
+    ;;
     c)
-	    columns_command="--maxcolseps $OPTARG"
-	    ;;
+      columns_command="--maxcolseps $OPTARG"
+    ;;
     l)
       classifier=$OPTARG
       if [ ! -f $classifier ]; then
-      echo "Model file $classifier does not exist"
-      usage
+        echo "Model file $classifier does not exist"
+        usage
       fi
       model_command='-m '$classifier
       if  $verbose ; then
-	      echo using model $classifier
+        echo using model $classifier
       fi
-      ;;
-     C)
+    ;;
+    C)
       compare_file=$OPTARG
       if [ ! -f $compare_file ]; then
-      echo "comparand file $compare_file does not exist"
-      usage
+        echo "comparand file $compare_file does not exist"
+        usage
       fi
       compare_command=' -C '$compare_file
       if $verbose ; then
-      	echo using comparand $compare_file
+        echo using comparand $compare_file
       fi
-      ;;
+    ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
       usage
-      ;;
+    ;;
     :)
       echo "Option -$OPTARG requires an argument." >&2
       usage
-      ;;
+    ;;
   esac
 done
 
-#Test the args 
+#Test the args
 shift $(($OPTIND - 1))
-if [ ! $# -gt 0 ]; then 
-	echo "no filename given"
-	usage	
+if [ ! $# -gt 0 ]; then
+  echo "no filename given"
+  usage
 fi
 
 if $verbose ; then
-	echo "outputfile $output_filename"
+  echo "outputfile $output_filename"
 fi
 
 if [ -z  $output_filename ]; then
-	echo "Output file $output_filename not set"
-	usage
+  echo "Output file $output_filename not set"
+  usage
 fi
 
-
+#Get stripped names to use in naming output, etc.
 fbname=`basename "$1"`
 if $verbose ; then
-	echo $fbname
+  echo $fbname
 fi
 
 barefilename=${fbname%.*}
 extension="${fbname##*.}"
 if $verbose ; then
-echo $barefilename
+  echo $barefilename
 fi
+
+#make a temporary directory for processing
 process_dir=`mktemp -d --suffix=$barefilename`
 process_file=$1
-#echo preprocessed? $image_is_preprocessed
+
+#Convert image to png if necessary
 if [ "$extension" != "png" ]
 then
-echo "converting $1 to $process_dir/$barefilename.png"
-eval convert $1 $process_dir/$barefilename.png $delete_string
-process_file=$process_dir/$barefilename.png
+  echo "converting $1 to $process_dir/$barefilename.png"
+  eval convert $1 $process_dir/$barefilename.png $delete_string
+  process_file=$process_dir/$barefilename.png
 fi
 
 if [[ -z "$fbname" ]]; then
-	usage
-	exit 1
+  usage
+  exit 1
 fi
+#Perform each step in the processing.
 #PG likes a 0.7 threshold, slightly darker than standard 0.5
 eval ocropus-nlbin $process_file -t 0.7 -o $process_dir $delete_string
-eval ocropus-gpageseg -b --maxseps 1 $columns_command  $process_dir'/????.bin.png' #$delete_string
-#end condition of process_dir existing
+eval ocropus-gpageseg -b --maxseps 1 $columns_command  $process_dir'/????.bin.png' 
 process_dir_for_classifier=`mktemp -d`
-cp -a $process_dir/* $process_dir_for_classifier 
+cp -a $process_dir/* $process_dir_for_classifier
 eval ocropus-rpred   $model_command $process_dir_for_classifier'/????/??????.bin.png' $delete_string
 eval ocropus-hocr $process_dir_for_classifier'/????.bin.png' -o $output_filename $delete_string
 rm -rf $process_dir_for_classifier > /dev/null
 rm -rf $process_dir > /dev/null
-#ocropus-visualize-results temp
 
